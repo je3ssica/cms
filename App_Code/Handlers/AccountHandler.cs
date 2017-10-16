0572cb5c23c1b5a116fcdb3055d1341070f26104
Strömberg <jessica.stromberg@cgi.com>
2017-10-16 08:23:43 +0200

    Add project files.

---
+++ App_Code/Handlers/AccountHandler.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Helpers;
+using WebMatrix.Data;
+
+/// <summary>
+/// Summary description for AccountHandler
+/// </summary>
+public class AccountHandler : IHttpHandler
+{
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+        var mode = context.Request.Form["mode"];
+        var username = context.Request.Form["accountName"];
+        var password1 = context.Request.Form["accountPassword1"];
+        var password2 = context.Request.Form["accountPassword2"];
+        var id = context.Request.Form["accountId"];
+        var email = context.Request.Form["accountEmail"];
+        var userRoles = context.Request.Form["accountRoles"];
+        var roles = userRoles.Split(',').Select(v => Convert.ToInt32(v));
+
+       if (mode == "delete")
+       {
+            Delete(username);
+       }
+        else
+        {
+            if(password1 != password2)
+            {
+                throw new Exception("Passwords do not match");
+            }
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("Email cannot be blank");
+            }
+            if(string.IsNullOrWhiteSpace(username))
+            {
+                throw new Exception("Username cannot be blank");
+            }
+            if(mode == "edit")
+            {
+                Edit(Convert.ToInt32(id), username, password1, email, roles);
+            }
+            else if(mode == "new")
+            {
+                Create(username, password1, email, roles);
+            }
+        }
+
+
+        context.Response.Redirect("~/admin/account");
+    }
+
+    private static void Create(string username, string password, string email, IEnumerable<int> roles)
+    {
+        if(string.IsNullOrWhiteSpace(password))
+        {
+            throw new Exception("Password cannot be blank");
+        }
+
+        var result = AccountRepository.Get(username);
+        if(result != null)
+        {
+            throw new HttpException(409, "User already exists");
+        }
+
+        AccountRepository.Add(username, Crypto.HashPassword(password), email, roles);
+    }
+
+    private static void Edit(int id, string username, string password, string email, IEnumerable<int> roles)
+    {
+        var result = AccountRepository.Get(id);
+        if (result == null)
+        {
+            throw new HttpException(404, "User does not exist");
+        }
+        var updatedPassword = result.Password;
+
+        if (!string.IsNullOrWhiteSpace(password))
+        {
+            updatedPassword = Crypto.HashPassword(password);
+        }
+        AccountRepository.Edit(id, result.Username, updatedPassword, email, roles);
+    }
+
+    private static void Delete(string username)
+    {
+        PostRepository.Remove(username);
+    }
+
+    private static string CreateSlug(string title)
+    {
+        title = title.ToLowerInvariant().Replace(" ", "-");
+        title = Regex.Replace(title, @"[^0-9a-z-]", string.Empty);
+
+        return title;
+    }
+}
