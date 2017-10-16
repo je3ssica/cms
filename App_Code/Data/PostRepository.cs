afec32c6ff45b0f6058ef0dc6ea5795e6ab1dbb3
je3ssica <je3ssica@gmail.com>
2017-10-16 15:48:22 +0200

    bug

--- App_Code/Data/PostRepository.cs
+++ App_Code/Data/PostRepository.cs
@@ -18,7 +18,7 @@ public class PostRepository
 
     public static IEnumerable<dynamic> GetPublishedPosts()
     {
-        var sql = "SELECT p.*, t.Id as TagId, t.Name as TagName, t.UrlFriendlyName as TagUrlFriendlyName, u.Username FROM Posts p LEFT JOIN PostsTagsMap m ON p.Id = m.PostId LEFT JOIN Tags t ON t.id = m.TagId INNER JOIN Users u ON u.Id = p.AuthorId WHERE DatePublished IS NOT NULL AND DatePublished < getDate()";
+        var sql = "SELECT p.*, t.Id as TagId, t.Name as TagName, t.UrlFriendlyName as TagUrlFriendlyName, u.Username as Username FROM Posts p LEFT JOIN PostsTagsMap m ON p.Id = m.PostId LEFT JOIN Tags t ON t.Id = m.TagId INNER JOIN Users u ON u.Id = p.AuthorId WHERE DatePublished IS NOT NULL AND DatePublished < getDate()";
 
         return DoGet(sql);       
         
